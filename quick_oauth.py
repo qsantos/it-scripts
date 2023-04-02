859e7d7f3e618c8b1f417c125226deb5bbbe2465
Quentin Santos <qsantos@qsantos.fr>
2023-04-02 22:07:53 +0200

    Add Python module for with OAuth-enabled APIs

---
+++ quick_oauth.py
@@ -0,0 +1,111 @@
+# inspired from https://www.camiloterevinto.com/post/oauth-pkce-flow-from-python-desktop
+import base64
+import hashlib
+import json
+import random
+import string
+import webbrowser
+from argparse import ArgumentParser
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from os import environ
+from sys import exit, stderr
+from typing import Iterator, Tuple
+from urllib import parse
+
+try:
+    import requests
+    from requests import Session
+    from oauthlib.oauth2 import WebApplicationClient
+except ModuleNotFoundError:
+    print("Please run:", file=stderr)
+    print("pip install oauthlib requests", file=stderr)
+    exit(1)
+
+
+class OAuthHttpServer(HTTPServer):
+    def __init__(self, *args, **kwargs):
+        HTTPServer.__init__(self, *args, **kwargs)
+        self.authorization_code = ""
+
+
+class OAuthHttpHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-Type", "text/html")
+        self.end_headers()
+        self.wfile.write("<script>window.close();</script>".encode())
+        parsed = parse.urlparse(self.path)
+        qs = parse.parse_qs(parsed.query)
+        self.server.authorization_code = qs["code"][0]
+
+
+def generate_code() -> Tuple[str, str]:
+    rand = random.SystemRandom()
+    verifier = "".join(rand.choices(string.ascii_letters + string.digits, k=128))
+
+    code_sha_256 = hashlib.sha256(verifier.encode()).digest()
+    b64 = base64.urlsafe_b64encode(code_sha_256)
+    challenge = b64.decode().rstrip("=")
+
+    return challenge, verifier
+
+
+def get_access_token(
+    authorize_uri: str,
+    token_uri: str,
+    client_id: str,
+    secret: str,
+    redirect_port: int,
+) -> str:
+    # generate challenge and verifier for PKCE
+    code_challenge, code_verifier = generate_code()
+
+    # get authorization code
+    redirect_uri = f"http://localhost:{redirect_port}/"
+    with OAuthHttpServer(("", redirect_port), OAuthHttpHandler) as httpd:
+        client = WebApplicationClient(client_id)
+        request_uri = client.prepare_request_uri(
+            uri=authorize_uri,
+            redirect_uri=redirect_uri,
+            scope="api",
+            state="",
+            code_challenge=code_challenge,
+            code_challenge_method="S256",
+        )
+        webbrowser.open_new(request_uri)
+        httpd.handle_request()
+        auth_code = httpd.authorization_code
+
+    # get token
+    data = {
+        "code": auth_code,
+        "client_id": client_id,
+        "grant_type": "authorization_code",
+        "scopes": "api",
+        "redirect_uri": redirect_uri,
+        "code_verifier": code_verifier,
+    }
+    response = requests.post(token_uri, data=data, verify=False)
+    return response.json()["access_token"]
+
+
+def oauth_session(
+    authorize_uri: str,
+    token_uri: str,
+    client_id: str,
+    secret: str,
+    redirect_port: int,
+) -> Session:
+    access_token = get_access_token(
+        authorize_uri=authorize_uri,
+        token_uri=token_uri,
+        client_id=client_id,
+        secret=secret,
+        redirect_port=redirect_port,
+    )
+    session = Session()
+    session.headers.update({"Authorization": "Bearer " + access_token})
+    return session
+
+
+__ALL__ = ["Session", "get_access_token", "oauth_session"]
